8e33f5e0f91ef846ef226953dafedab6b6c09192
Leul <leulzewduchanie@gmail.com>
2024-07-28 03:38:42 -0700

    adjusted for desktop view

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// let hm_btn = document.getElementById('hm_btn')
-// hm_btn.addEventListener('click',()=>{
-//     let side = document.querySelector('aside')
-//     if(side.style.visibility == "visible"){
-//     side.style.visibility="hidden";
-//     side.style.opacity= "0"
-//   }else{
-//     side.style.visibility="visible";
-//     side.style.opacity = "1";
-//   }
-// })
 
 window.addEventListener('load',()=>{
   getInfo('new york').then(data=>{
